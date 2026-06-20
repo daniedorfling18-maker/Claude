073410cce59814bdd24ed60fa00bfe240e34f024
daniedorfling18-maker <daniedorfling18@gmail.com>
2026-06-20 07:53:07 +0200

    Export defensive game theory overlay

---
+++ src/superbru_score_engine/game_theory/__init__.py
@@ -0,0 +1,15 @@
+from superbru_score_engine.game_theory.defensive import (
+    build_chaser_distribution,
+    build_leader_candidates,
+    estimate_candidate_ev,
+    evaluate_match,
+    run_defensive_model,
+)
+
+__all__ = [
+    "build_chaser_distribution",
+    "build_leader_candidates",
+    "estimate_candidate_ev",
+    "evaluate_match",
+    "run_defensive_model",
+]

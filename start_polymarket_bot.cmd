de846bbb97e63ed1e576f3473fed2c2d84e7b6e6
daniedorfling18-maker <daniedorfling18@gmail.com>
2026-06-27 22:02:25 +0200

    Use rotate scan defaults in one-click Polymarket launcher

--- start_polymarket_bot.cmd
+++ start_polymarket_bot.cmd
@@ -1,5 +1,7 @@
 @echo off
 setlocal
 cd /d "%~dp0"
+if "%POLYMARKET_SCAN_QUERY_MODE%"=="" set POLYMARKET_SCAN_QUERY_MODE=rotate
+if "%POLYMARKET_MAX_SCAN_QUERIES%"=="" set POLYMARKET_MAX_SCAN_QUERIES=1
 powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0scripts\start_polymarket_local_live.ps1" -ForceRestart
 pause
